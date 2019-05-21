5f4bdc97b7f280008c8f91b14b17e3fc30da5f58
rafaelkhasanov <rap1raf@gmail.com>
2019-05-21 20:24:16 +0300

    Add project files.

---
+++ SyncDelegateReview/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SyncDelegateReview
+{
+    public delegate int BinaryOp(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sync Delegate Review");
+            //Вывести идентификатор выполняющегося потока
+            Console.WriteLine($"Main() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+
+            //Вызвать Add() в синхронном режиме
+            BinaryOp b = new BinaryOp(Add);
+            //Можно было бы написать b.Invoke(10, 10);
+            int answer = b(10, 10);
+
+            //Эти строки не выполняться до тех пор пока не завершится метод Add()
+            Console.WriteLine("Doing more work in Main()!");
+            Console.WriteLine("10 + 10 is {0}.", answer);
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            //Вывести идентификатор выполняющегося потока
+            Console.WriteLine($"Add() invoked on thread {Thread.CurrentThread.ManagedThreadId}");
+            //Сделать паузу для моделирования длительной операции.
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
